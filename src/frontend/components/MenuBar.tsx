909391dd2efbd10f52e3857ee0b4e62d108eb8df
Andi <anditan2014@gmail.com>
2022-01-31 23:22:43 +1100

    Using Navbar component from react-bootstrap

--- src/frontend/components/MenuBar.tsx
+++ src/frontend/components/MenuBar.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
-import { Navbar, Button } from "react-bootstrap";
+import { Nav, Navbar, Button } from "react-bootstrap";
 
 export default function MenuBar() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <Navbar variant="dark">asdf</Navbar>
+        <Navbar variant="light">
+          <Nav>
+            <Nav.Link href="#/">Home</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     </div>
   );
