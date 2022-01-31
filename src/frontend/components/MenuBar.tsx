ff1a938e6219e1674a8d8f3013c6d9e74d378512
Andi <anditan2014@gmail.com>
2022-01-31 22:24:44 +1100

    Using bootstrap. Added basic container and Navbar component

---
+++ src/frontend/components/MenuBar.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Navbar, Button } from "react-bootstrap";
+
+export default function MenuBar() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Navbar variant="dark">asdf</Navbar>
+      </div>
+    </div>
+  );
+}
