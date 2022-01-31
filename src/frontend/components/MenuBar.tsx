8ef299a20b7b6d674d8689af2bfd0644634c0ba2
Andi <anditan2014@gmail.com>
2022-01-31 23:56:47 +1100

    Created a simple navigation bar

--- src/frontend/components/MenuBar.tsx
+++ src/frontend/components/MenuBar.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { Nav, Navbar, Button } from "react-bootstrap";
+import { BsStarFill } from "react-icons/bs";
+import { IconContext } from "react-icons/lib";
 
 export default function MenuBar() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <Navbar variant="light">
+        <Navbar variant="dark" bg='dark'>
+          <h3 style = {{color:'white'}}>Central Planning <IconContext.Provider value = {{color:'red'}}><BsStarFill></BsStarFill></IconContext.Provider></h3>
           <Nav>
-            <Nav.Link href="#/">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
           </Nav>
         </Navbar>
       </div>
