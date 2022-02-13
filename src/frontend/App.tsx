5e24956e62d32c9eb4b4bfeaa1b510a065d8fa9e
Andi <anditan2014@gmail.com>
2022-02-14 04:53:19 +1100

    Added vertical sidebar

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import MenuBar from './components/MenuBar'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MenuBar from "./components/MenuBar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menuBarActive, setMenuBarStatus] = useState<boolean>(false);
+  var menuBarClass = menuBarActive ? "menu-bar-open" : "menu-bar-closed";
+  var menuColumns = menuBarActive ? "col-3 p-0" : "col-1 p-0";
+  var appColumns = menuBarActive ? "col-9 p-0" : "col-11 p-0";
 
-  return (<MenuBar></MenuBar>) 
+  return (
+    <Router>
+      <div className="container-fluid">
+        <div className="row">
+          <div className={menuColumns}>
+            <div id="menu-bar" className={menuBarClass}>
+              <MenuBar menuBarActive={menuBarActive} />
+            </div>
+          </div>
+          <div className={appColumns}>
+            <Routes></Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/frontend/components/MenuBar.tsx
+++ src/frontend/components/MenuBar.tsx
@@ -1,12 +1,62 @@
-import { useEffect } from "react";
-import { Nav, Navbar, Button } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import { BsStarFill } from "react-icons/bs";
 import { IconContext } from "react-icons/lib";
+import "./MenuBar.css";
+import { MdOutlineArrowBack, MdOutlineArrowForward } from "react-icons/md";
+import { AiOutlineMenu } from "react-icons/ai";
 
-export default function MenuBar() {
+type MenuBarProps = {
+  menuBarActive: boolean;
+};
+
+/* TODO: Store all buttons for menu bar in an array.
+Then, map each array element to a dive for the justify-content-center
+Maybe change type to be Array<FC> instead of Array<any>
+Finally, pass a function to change the value of menuBarActive as a prop*/
+export default function MenuBar({ menuBarActive }: MenuBarProps) {
+  // Assume the following 2 arrays are of the same size
+  var menuButtons: Array<any> = menuBarActive ? ["Home"] : ["H"];
+  const links = ["/"];
+
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <Navbar variant="dark" bg="dark">
+      <div id="menu-bar">
+        <div className="d-flex flex-column">
+          <div className="d-flex justify-content-center logo">
+            <IconContext.Provider value={{ color: "#DC3545", size: "28px"}}>
+              <BsStarFill></BsStarFill>
+            </IconContext.Provider>
+          </div>
+          <div className="d-flex justify-content-center">
+            <Button id="menu-status-button" variant="danger">
+              <AiOutlineMenu />
+            </Button>
+          </div>
+          {renderButtons(menuButtons, links)}
+        </div>
+      </div>
+    </Navbar>
+  );
+}
+
+function renderButtons(buttons: Array<any>, links: Array<string>) {
+  var counter = 0;
+  return buttons.map((button) => {
+    var toReturn = (
+      <div className="d-flex justify-content-center">
+        <Nav>
+          <Nav.Link href={links[counter]}>{button}</Nav.Link>
+        </Nav>
+      </div>
+    );
+    counter += 1;
+    return toReturn;
+  });
+}
+
+/* 
         <Navbar variant="dark" bg="dark">
           <h4 style={{ color: "white" }}>
             Central Planning{" "}
@@ -18,7 +68,4 @@ export default function MenuBar() {
             <Nav.Link href="/">Home</Nav.Link>
           </Nav>
         </Navbar>
-      </div>
-    </div>
-  );
-}
+        */
